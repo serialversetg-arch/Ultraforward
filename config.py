454a0c36ce78f49e4949e2f5654e63560024a404
serialversetg-arch <serialversetg@gmail.com>
2026-03-01 00:43:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 1234567 
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+SOURCE_CHAT = -100xxxxxxxxx 
+TARGET_CHAT = -100xxxxxxxxx 
+
+OLD_TAG = "TvShowHub.mp4"
+NEW_TAG = "Hindi_Tv_Verse.mp4"
